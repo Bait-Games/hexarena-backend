5db11e5686bc464d33f0dc746fed73c4f01139d5
Annonymus <terehm@usi.ch>
2021-07-30 15:14:54 +0200

    Create engine directory and own it
    Also add enough code to compile, a cmakelists file and all the constants used by the engine.

---
+++ engine/engine.h
@@ -0,0 +1,14 @@
+#ifndef HEXARENA_BACKEND_ENGINE_H
+#define HEXARENA_BACKEND_ENGINE_H
+
+#include "constants.h"
+
+class Engine {
+
+public:
+    void init() {
+
+    }
+};
+
+#endif //HEXARENA_BACKEND_ENGINE_H
